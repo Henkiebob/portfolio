032812b2370acdc2b15005ac6ee5343384737d9d
Henkiebob <tjerk.dijkstra@gmail.com>
2014-08-02 17:33:06 +0200

    Lots of work has been done

--- javascripts/site.js
+++ javascripts/site.js
@@ -13,4 +13,25 @@ $( document ).ready(function() {
 		$(this).next().show();
 	});
 	
+	$( ".avatar" ).click(function() {
+		var path = 'images/';
+		var objects = [
+			path + 'tjerk-jippe-kakker.jpg',
+			path + 'tjerk-jippe-baby.jpg',
+			path + 'tjerk-jippe.jpg',
+			path + 'tjerk-jippe-batman.jpg',
+			path + 'tjerk-jippe-bane.jpg',
+			path + 'tjerk-jippe-pirate.jpg'
+		];
+
+		shuffle(objects);	
+		$(this).attr('src', objects[0]);
+	});
+
 });
+
+// shuffle for header
+function shuffle(o){ //v1.0
+	for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+	return o;
+};
